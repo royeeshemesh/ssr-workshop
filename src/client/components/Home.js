bd4f553bbde1c66ae97ec9a86613248c90c626d3
Royee Shemesh <Royee_Shemesh@intuit.com>
2018-06-21 20:12:14 +0300

    01-fetching-data-client (#1)
    01-fetching-data-client

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,10 +1,57 @@
-import React, {Component} from 'react';
+import axios from 'axios';
+import React, { Component } from 'react';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  async componentDidMount() {
+    const users = await axios({
+      method: 'GET',
+      url: 'https://jsonplaceholder.typicode.com/users'
+    });
+
+    this.setState({
+      users: users.data
+    });
+  }
+
   render() {
+    const getUsersRows = () => {
+      if (!this.state.users) {
+        return null;
+      }
+      return this.state.users.map((user, index)=>{
+        return (
+          <tr key={index}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+          </tr>
+        )
+      })
+    };
+
     return (
       <div>
         <h1>Welcome to SSR Workshop</h1>
+        <table border="1">
+          <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+          </tr>
+          </thead>
+          <tbody>
+          {getUsersRows()}
+          </tbody>
+        </table>
       </div>
     );
   }
