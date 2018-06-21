a378b5071f49f161fece00653a099d0e2fc343a8
Royee Shemesh <Royee_Shemesh@intuit.com>
2018-06-21 20:38:29 +0300

    02-server-bundle (#2)
    * Basic client data fetch
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT

---
+++ webpack.server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpackNodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+
+  entry: './src/server.js',
+
+  output: {
+    filename: 'bundled_server.js',
+    path: path.resolve(__dirname, 'build')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      }
+    ]
+  },
+
+  externals: [webpackNodeExternals()]
+};
