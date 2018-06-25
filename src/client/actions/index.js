391070f5583f0fcacee0dd79c02e9892e9b671e3
Royee Shemesh <royee_shemesh@intuit.com>
2018-06-25 12:27:26 -0700

    SAVEPOINT

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const FETCH_USERS = 'fetch_users';
 export const fetchUsers = ()=> async dispatch => {
-  console.info(6, 'fetch users action created');
   const users = await axios({
     method: 'GET',
     url: 'https://jsonplaceholder.typicode.com/users'
