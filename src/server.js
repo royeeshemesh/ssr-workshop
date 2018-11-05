d7f5188fc2c9e4dc4af290b646337d9d3e24db05
Royee Shemesh <royee_shemesh@intuit.com>
2018-11-05 14:43:11 +0200

    SAVEPOINT

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { matchRoutes } from 'react-router-config';
+import {renderToString} from "react-dom/server";
+import {matchRoutes, renderRoutes} from 'react-router-config';
+import { StaticRouter } from 'react-router-dom';
+import React from 'react';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 import Routes from './client/Routes';
-import renderer from './helpers/renderer';
-import createStore from './helpers/createStore';
+import reducers from './client/reducers';
 
 const app = express();
 
@@ -11,10 +16,33 @@ app.use(express.static('public'));
 
 // listen to root request
 app.get('*', async (req, res) => {
-  const store = createStore();
+  const store = createStore(reducers, {}, applyMiddleware(thunk));
   const promises = matchRoutes(Routes, req.path).map(({route})=>route.fetchData ? route.fetchData(store) : Promise.resolve(null));
   await Promise.all(promises);
-  res.send(renderer(req, store));
+
+  const content = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.path} context={{}}>
+        <div>
+          {renderRoutes(Routes)}
+        </div>
+      </StaticRouter>
+    </Provider>
+  );
+
+  res.send(`
+<html>
+<body>
+    <div id="root">${content}</div>
+    <script>
+      window.INITIAL_STATE = ${JSON.stringify(store.getState())};
+    </script>
+    <script src="bundle.js"></script>
+</body>
+</html>
+`);
+
+
 });
 
 // start server
